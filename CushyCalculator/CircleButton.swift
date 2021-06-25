aa086072f437148d917c71e3fe8a59e4063ad8bd
naldal <cec7378@gmail.com>
2021-06-26 00:06:17 +0900

    basic operation logic

---
+++ CushyCalculator/CircleButton.swift
@@ -0,0 +1,8 @@
+//
+//  CircleButton.swift
+//  CushyCalculator
+//
+//  Created by 송하민 on 2021/06/25.
+//
+
+import Foundation
---
+++ CushyCalculator/RoundButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundButton.swift
+//  CushyCalculator
+//
+//  Created by 송하민 on 2021/06/24.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
